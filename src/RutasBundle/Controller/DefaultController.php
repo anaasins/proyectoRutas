ec8b038b4254278bf1c17c9961a4c20418584374
Ana Asins <asinsanna@gmail.com>
2017-10-06 11:59:59 +0200

    rutas hechas

--- src/RutasBundle/Controller/DefaultController.php
+++ src/RutasBundle/Controller/DefaultController.php
@@ -14,4 +14,36 @@ class DefaultController extends Controller
     {
         return $this->render('RutasBundle:Default:index.html.twig');
     }
+
+    /**
+     * @Route("/descripcionRuta, name=descripcionRuta")
+     */
+    public function descripcionRutaAction()
+    {
+        return $this->render('RutasBundle:Default:descripcionRuta.html.twig');
+    }
+
+    /**
+     * @Route("/anadirRuta, name=anadirRuta")
+     */
+    public function anadirRutaAction()
+    {
+        return $this->render('RutasBundle:Default:anadirRuta.html.twig');
+    }
+
+    /**
+     * @Route("/editarRuta, name=editarRuta")
+     */
+    public function editarRutaAction()
+    {
+        return $this->render('RutasBundle:Default:editarRuta.html.twig');
+    }
+
+    /**
+     * @Route("/rutasPropias, name=rutasPropias")
+     */
+    public function rutasPropiasAction()
+    {
+        return $this->render('RutasBundle:Default:rutasPropias.html.twig');
+    }
 }
