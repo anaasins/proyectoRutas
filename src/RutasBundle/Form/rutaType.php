12e63fc7b5567bb229578f43787ce9717309a6d8
Ana <asinsanna@gmail.com>
2017-11-20 18:24:23 +0100

    proyecto 05 crud acabado

--- src/RutasBundle/Form/rutaType.php
+++ src/RutasBundle/Form/rutaType.php
@@ -6,6 +6,10 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class rutaType extends AbstractType
 {
@@ -15,17 +19,17 @@ class rutaType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('nombre')
-        ->add('fecha')
+        ->add('nombre', TextType::class, array('attr' => array('class' => 'w3-input'),))
+        ->add('fecha', DateType::class)
         ->add('hora')
-        ->add('descripcion')
-        ->add('lugar')
-        ->add('duracion')
-        ->add('organizador')
-        ->add('nivel')
-        ->add('imagenes')
+        ->add('descripcion', TextareaType::class, array('attr' => array('class' => 'w3-input'),))
+        ->add('lugar', TextType::class, array('attr' => array('class' => 'w3-input'),))
+        ->add('duracion', TextType::class, array('attr' => array('class' => 'w3-input'),))
+        ->add('organizador', TextType::class, array('attr' => array('class' => 'w3-input'),))
+        ->add('nivel', TextType::class, array('attr' => array('class' => 'w3-input'),))
+        ->add('imagenes', TextareaType::class, array('attr' => array('class' => 'w3-input'),))
         ->add('usuario')
-        ->add('Enviar', SubmitType::class)
+        ->add('Enviar', SubmitType::class, array('attr' => array('class' => 'w3-btn w3-green w3-round-large w3-xlarge'),))
         ;
     }
 
